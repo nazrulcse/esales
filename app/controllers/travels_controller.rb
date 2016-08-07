59472e0e60c48ec947353ff12018768e6ccd5bc5
nazrul <nazrulku07@gmail.com>
2016-08-07 21:01:24 +0600

    add travel show page

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -1,5 +1,17 @@
 class TravelsController < ApplicationController
+  before_action :set_travel
+
   def index
     @travels = Travel.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_travel
+    @travel = Travel.find_by_slug(params[:id])
   end
 end
